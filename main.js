b6a92a2ad784263485c9f593ac72af4aa2189323
kamyarrazi14999 <kami6535@gmail.com>
2025-07-03 02:06:52 +0330

    Refactor loadAudio function and improve play/pause button event handling

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ const singerName = document.querySelector(".singer-name");
 const musicName = document.querySelector(".music-name");
 // Event listeners for play/pause, next, and previous buttons
 // Play/Pause button
+
 const playlist = [
   { singerName: 'Abdoul Pit', musicName: 'singer name 1', audioSrc: './assets/1-music.mp3', coverSrc: 'assets/1-cover.jpg' },
   { singerName: 'json jafari', musicName: 'singer name 3', audioSrc: './assets/2-music.mp3', coverSrc: 'assets/2-cover.jpg' },
@@ -19,6 +20,7 @@ const playlist = [
 ];
 let currentIndex = 0;
 let isPlaying = false;
+// load the current track
 const loadAudio = () => {
   const track = playlist[currentIndex];
   coverImage.src = track.coverSrc;
@@ -28,7 +30,24 @@ const loadAudio = () => {
 if(isPlaying) {
     audio.play();
   }
+  playpauseButton.addEventListener("click", () => {
+    const playIcon = playpauseButton.firstElementChild;
+    if (isPlaying) {
+      audio.pause();
+      // Change the icon to play
+      playIcon.classList.replace("fa-pause", "fa-play");
+      
+    
+    } else {
+      audio.play();
+      // Change the icon to pause
+      
+      playIcon.classList.replace("fa-play", "fa-pause");
+      
+    
+    }
+    isPlaying = !isPlaying;
+  });
 }
 loadAudio();
-
 
